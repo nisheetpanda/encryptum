1d140f0f88b56665f31b0c9b85ab29cf7ac8c9f2
AidenW0ng <93459706+AidenW0ng@users.noreply.github.com>
2022-01-14 12:14:09 -0800

    sussus amongus
    added testing of a fire reader and a file writer

--- encryptum.java
+++ encryptum.java
@@ -1,7 +1,23 @@
 import java.util.*;
+import java.io.FileWriter;
+import java.io.FileReader;
 
 public class encryptum {
-   public static void main(String[] args) {
-  
-   }
+   public static void main(String[] args) throws Exception{
+        try {
+            FileWriter fw = new FileWriter("testout.txt");
+            fw.write("this is a test");
+            fw.close();
+        }
+        catch(Exception e){System.out.println(e);} 
+                System.out.println("it work");
+
+        FileReader fr = new FileReader("testout.txt");
+        int i = 0;
+        while(i != -1) {
+            i = fr.read();
+            if(i != -1)
+                System.out.print((char)i);
+        }
+    }
 }
