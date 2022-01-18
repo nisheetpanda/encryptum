56b703e95f1c36c14807d0941b45721176d4e5bf
DheerajSom <63675896+Dheerajsom@users.noreply.github.com>
2022-01-18 12:07:23 -0800

    Update encryptum.java

--- encryptum.java
+++ encryptum.java
@@ -21,3 +21,26 @@ public class encryptum {
         }
     }
 }
+           
+ ////////////////////////////////////////////////////////
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class notes {
+  public static void main(String[] args) {
+    try {
+      File readFile = new File("read.txt");
+      Scanner myReader = new Scanner(readFile);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
+      
